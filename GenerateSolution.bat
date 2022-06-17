a6ce79cdfe9d11e95fad3f4c46f87f3d77e4adf0
Oxi <33568643+oxi-dev0@users.noreply.github.com>
2022-06-17 08:40:02 +0100

    Update GenerateSolution.bat

--- GenerateSolution.bat
+++ GenerateSolution.bat
@@ -1,2 +1,7 @@
-call vendor\bin\premake\premake5.exe vs2022
+if exist vendor/sfml/lib (
+  call vendor\bin\premake\premake5.exe vs2022 
+) else (
+  echo The repo is missing needed libraries in vendor/. Please make sure you clone the repo recursively as it utilises git submodules.
+)
+
 PAUSE
